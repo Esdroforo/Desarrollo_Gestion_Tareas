0481d894f813d06d2707e0e7a0e6754912f94e75
Esdroforo <mmesdras@hotmail.com>
2026-02-18 23:27:53 -0600

    Agregadas funciones de agregar, listar y completar tareas

--- tarea2.py
+++ tarea2.py
@@ -47,3 +47,35 @@ class GestorTareas:
                 print(f"[DEBUG] Tarea completada: {self.tareas[indice]['descripcion']}")
         else:
             print("Índice no válido.")
+
+        # Programa principal
+if __name__ == "__main__":
+    config = Configuracion(modo_debug=True)  # Se crea la única instancia
+    gestor = GestorTareas()
+
+    while True:
+        print("\n--- Menú de Gestión de Tareas ---")
+        print("1. Agregar tarea")
+        print("2. Listar tareas")
+        print("3. Completar tarea")
+        print("4. Salir")
+
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            descripcion = input("Escribe la descripción de la tarea: ")
+            gestor.agregar_tarea(descripcion)
+        elif opcion == "2":
+            gestor.listar_tareas()
+        elif opcion == "3":
+            gestor.listar_tareas()
+            try:
+                num = int(input("Número de la tarea a completar: ")) - 1
+                gestor.completar_tarea(num)
+            except ValueError:
+                print("Debes ingresar un número válido.")
+        elif opcion == "4":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida.")
